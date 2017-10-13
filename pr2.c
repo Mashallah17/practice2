a16849680865a68fc722f4535e213d2d9b48da61
Mashallah17 <32356001+Mashallah17@users.noreply.github.com>
2017-10-13 16:11:28 +0300

    Update pr2.c

--- pr2.c
+++ pr2.c
@@ -18,7 +18,7 @@ int main(){
 	}
 	/*Вывод массива*/
 	for (i = 0; i < n; i++){
-		for (j = 0; j < m; j++)	
+		for (j = 0; j < m; j++)
 			printf("%d  ",  a[i][j]);
 		printf("\n");
 	}
@@ -38,8 +38,8 @@ int main(){
 	a[n-1]=b;
 
 	/*Нахождение наименьшего элемента двумерного массива*/
-	int nomberI = 0, nomberJ = 0, min = a[0][0]; 
-	for (int i = 0; i < n; i++)
+	int nomberI = 0, nomberJ = 0, min = a[0][0];
+	for (i = 0; i < n; i++)
 		for (j = 0; j < m; j++)
 			if (min > a[i][j]){
 				min = a[i][j];
@@ -56,11 +56,57 @@ int main(){
 	free(a[n-1]);
 	n--;
 
+	/*Удаление столбца*/
+
+	for (j = nomberJ; j < m-1; j++){
+        for (i = 0; i < n; i++){
+            a[i][j]=a[i][j+1];
+        }
+	}
+	m--;
+    /*Распечатка*/
 	for (i = 0; i < n; i++){
-		for (j = 0; j < m; j++)	
+		for (j = 0; j < m; j++)
 			printf("%d  ",  a[i][j]);
 		printf("\n");
 	}
+
+    /*Ввод массива с клавиатура*/
+	printf("Enter an array size %d x %d\n", n, m);
+	free(b);
+	int **d;
+	d = (int**)malloc(m*sizeof(int*));
+	for (i = 0; i < m; i++){
+        d[i] = (int*)malloc(n*sizeof(int));
+        for(j = 0; j < n; j++)
+            scanf("%d", b);
+	}
+
+    /*Умножение матрицы А на Б*/
+    int **c;
+    c = (int**)malloc(n*sizeof(int*));
+    for (i = 0; i < n; i++){
+      c[i]=(int*)malloc(n*sizeof(int));
+      for(j = 0; j < n; j++){
+        c[i][j]=0;
+        for(l = 0; l < n; l++)
+            c[i][j] += a[j][l]*d[l][j];
+     }
+    }
+
+    /*Распечатка*/
+	for (i = 0; i < n; i++){
+		for (j = 0; j < n; j++)
+			printf("%d  ",  a[i][j]);
+		printf("\n");
+	}
+
+    /*Освобождение памяти*/
+    for (i = 0; i < n; i++){
+        free(a[i]);
+        free(d[i]);
+        free(c[i]);
+    }
 
 	return 0;
 
